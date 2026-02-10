7ed7613510434ffcceb5676b38d8b50c58c89387
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-10 19:04:30 +0530

    Auto-commit: sections/about.js at 2/10/2026, 7:04:30 PM

--- sections/about.js
+++ sections/about.js
@@ -9,9 +9,7 @@ export function initAbout() {
     heading.textContent = 'About Me';
 
     const text = document.createElement('p');
-    text.textContent = 'Hello! My name is Kavin I’m a 3rd-year Computer Science and Design student passionate about building intelligent and creative solutions. I love blending design with code—whether it's developing AI models, animating in Blender, or building interactive experiences in Unity.
-
-Recently, I passed the NAT N5 Japanese language test and I’m preparing for NAT N4. I’m interested in exploring opportunities that combine tech and creativity.
+    text.textContent = 'Hello! My name is Kavin I’m a 3rd-year Computer Science and Design student passionate about building intelligent and creative solutions. I love blending design with code—whether it\'s developing AI models, animating in Blender, or building interactive experiences in Unity.Recently, I passed the NAT N5 Japanese language test and I’m preparing for NAT N4. I’m interested in exploring opportunities that combine tech and creativity.
 
 I'm currently building my skills in AI, Python, and 3D development and am looking forward to contributing to exciting projects, collaborating with others, and learning more through internships or freelance work.
 
