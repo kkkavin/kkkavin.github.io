a2ea0c7c9af56f59428e2a6d64d984d433cc064b
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-10 22:31:05 +0530

    Auto-commit: sections/skills.js at 2/10/2026, 10:31:05 PM

--- sections/skills.js
+++ sections/skills.js
@@ -12,7 +12,7 @@ export function initSkills() {
     grid.className = 'grid';
     grid.style.gridTemplateColumns = 'repeat(auto-fit, minmax(150px, 1fr))';
 
-    const skills = ['Python', 'C', 'Java', 'HTML5 & CSS3', 'CSS', 'HTML5 & CSS3', 'Python', 'Git'];
+    const skills = ['Python', 'C', 'Java', 'HTML5 & CSS3', 'JavaScript', 'HTML5 & CSS3', 'Python', 'Git'];
 
     skills.forEach(skill => {
         const card = document.createElement('div');
